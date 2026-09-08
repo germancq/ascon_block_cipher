ae5874927ca9710b09c0ce9e642d1fc0a38c2a91
germancq <germancq@debianGCQ.dte.us>
2026-09-08 09:58:19 +0200

    dev associated data phase

--- ecb_mode/cocotb/ascon_processing_associated_data_phase_test.py
+++ ecb_mode/cocotb/ascon_processing_associated_data_phase_test.py
@@ -214,7 +214,7 @@ async def test(dut, index=0):
         S,
         int(dut.b.value),
         int(dut.rate.value),
-        A_value.to_bytes((n * int(dut.rate.value)), "little"),
+        A_value.to_bytes((n * int(dut.rate.value)), "big"),
     )
 
     setup_dut(dut, S_init)
