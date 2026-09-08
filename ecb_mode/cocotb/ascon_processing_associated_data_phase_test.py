4d5a2aa4bb092376598f71764a2ad57f9515acd8
germancq <germancq@debianGCQ.dte.us>
2026-09-08 10:52:09 +0200

    dev associated data phase

--- ecb_mode/cocotb/ascon_processing_associated_data_phase_test.py
+++ ecb_mode/cocotb/ascon_processing_associated_data_phase_test.py
@@ -156,8 +156,8 @@ async def test_associated_data(dut, expected_State, A_array, num_blocks):
         dut.FINAL_STEP_1.value
     ), f"ERROR STATE IN TEST, STATE={int(dut.current_state.value)}"
 
-    assert dut.state_w[4].value == 1, f"ERROR Write signal"
-    S_dut[4] = int(dut.state_din[4].value)
+    assert dut.state_w[0].value == 1, f"ERROR Write signal"
+    S_dut[0] = int(dut.state_din[0].value)
 
     await n_cycles_clock(dut, 1)
 
