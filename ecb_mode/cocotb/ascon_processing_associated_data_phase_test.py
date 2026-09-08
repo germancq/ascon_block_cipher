2a485add337ffb21432ae85461f757582d76fbc2
germancq <germancq@debianGCQ.dte.us>
2026-09-08 10:48:49 +0200

    dev associated data phase

--- ecb_mode/cocotb/ascon_processing_associated_data_phase_test.py
+++ ecb_mode/cocotb/ascon_processing_associated_data_phase_test.py
@@ -50,8 +50,8 @@ async def test_associated_data(dut, expected_State, A_array, num_blocks):
     for i in range(0, 5):
         S_dut[i] = int(dut.state_dout[i].value)
 
-    await n_cycles_clock(dut, 1)
     dut.block_i.value = A_array[0]
+    await n_cycles_clock(dut, 1)
     for n in range(0, num_blocks):
         dut._log.info(
             "cycle {0} with numblocks = {2} in test with data={1}".format(
