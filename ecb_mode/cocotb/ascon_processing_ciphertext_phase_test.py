9fbdd8ebb2641595f60c2017e04241ad57269c4a
germancq <germancq@debianGCQ.dte.us>
2026-09-10 15:38:07 +0200

    dev ciphertext phase

---
+++ ecb_mode/cocotb/ascon_processing_ciphertext_phase_test.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : ascon_processing_ciphertext_phase_test.py
+# Author            : German C.Quiveu <germancq@dte.us.es>
+# Date              : 10.09.2026
+# Last Modified Date: 10.09.2026
+# Last Modified By  : German C.Quiveu <germancq@dte.us.es>
+import os
+import random
+import sys
+
+import cocotb
+import numpy as np
+from ascon import *
+from cocotb.clock import Clock
+from cocotb.triggers import FallingEdge, RisingEdge, Timer
+
+CLK_PERIOD = 20
+
+
+def setup_dut(dut, initial_state):
+    cocotb.start_soon(Clock(dut.clk, CLK_PERIOD, unit="ns").start())
+    dut.rst.value = 0
+    dut.start.value = 0
+    dut.p_impl_end.value = 0
+    dut.feed_block.value = 0
+    dut.last_block.value = 0
+    dut.block_i.value = 0
+    for i in range(0, 5):
+        dut.state_dout[i].value = initial_state[i]
+
+
+async def rst_function_test(dut):
+    dut.rst.value = 1
+    await n_cycles_clock(dut, 1)
+    assert int(dut.current_state.value) == int(
+        dut.IDLE.value
+    ), f"ERROR STATE IN RST, STATE={int(dut.current_state.value)}"
+    await n_cycles_clock(dut, 10)
+    assert int(dut.current_state.value) == int(
+        dut.IDLE.value
+    ), f"ERROR STATE IN RST, STATE={int(dut.current_state.value)}"
+
+
+async def test_ciphertext_data(dut, expected_State, C_array, num_blocks):
+    dut.rst.value = 0
+    dut.start.value = 1
+    S_dut = [0, 0, 0, 0, 0]
+
+    P_array = [0] * (num_blocks)
+
+    for i in range(0, 5):
+        S_dut[i] = int(dut.state_dout[i].value)
+
+    dut.block_i.value = C_array[0]
+    await n_cycles_clock(dut, 1)
+    for n in range(0, num_blocks):
+        dut._log.info(
+            "cycle {0} with numblocks = {2} in test with data={1}".format(
+                n, hex(C_array[n]), num_blocks
+            )
+        )
+
+        if n == num_blocks - 1:
+            dut.last_block.value = 1
+        else:
+            dut.last_block.value = 0
+
+        assert int(dut.current_state.value) == int(
+            dut.XOR_DATA.value
+        ), f"ERROR STATE IN TEST, STATE={int(dut.current_state.value)}"
+
+        assert dut.state_w[0].value == 1, f"ERROR Write signal"
+        S_dut[0] = int(dut.state_din[0].value)
+
+        dut._log.info("S0 is {0}".format(hex(dut.state_din[0].value)))
+        dut._log.info("block_i is {0}".format(hex(dut.block_i.value)))
+        dut._log.info("r_block_o_din is {0}".format(
+            hex(dut.r_block_o_din.value)))
+
+        if int(dut.rate.value) == 16:
+            assert dut.state_w[1].value == 1, f"ERROR Write signal"
+            S_dut[1] = int(dut.state_din[1].value)
+
+        await n_cycles_clock(dut, 1)
+
+        P_array[n] = int(dut.block_o.value)
+        dut._log.info("block_o is {0}".format(hex(dut.block_o.value)))
+
+        assert int(dut.current_state.value) == int(
+            dut.ASCON_PERMUTATION_B_0.value
+        ), f"ERROR STATE IN TEST, STATE={int(dut.current_state.value)}"
+
+        ascon_permutation(S_dut, int(dut.b.value))
+
+        await n_cycles_clock(dut, 1)
+
+        assert int(dut.current_state.value) == int(
+            dut.ASCON_PERMUTATION_B_1.value
+        ), f"ERROR STATE IN TEST, STATE={int(dut.current_state.value)}"
+        dut.p_impl_end.value = 1
+
+        for i in range(0, 5):
+            dut.state_dout[i].value = S_dut[i]
+
+        await n_cycles_clock(dut, 1)
+
+        assert int(dut.current_state.value) == int(
+            dut.END_FEED.value
+        ), f"ERROR STATE IN TEST, STATE={int(dut.current_state.value)}"
+
+        dut._log.info(dut.last_block.value)
+
+        await n_cycles_clock(dut, 1)
+
+        if dut.last_block.value == 1:
+            break
+        else:
+            assert int(dut.current_state.value) == int(
+                dut.END_FEED_0.value
+            ), f"ERROR STATE IN TEST, STATE={int(dut.current_state.value)}"
+
+            dut.feed_block.value = 1
+            dut.block_i.value = P_array[n + 1]
+
+            await n_cycles_clock(dut, 1)
+
+            dut.feed_block.value = 0
+
+    assert int(dut.current_state.value) == int(
+        dut.FINAL_STEP_0.value
+    ), f"ERROR STATE IN TEST, STATE={int(dut.current_state.value)}"
+
+    assert dut.state_w[0].value == 1, f"ERROR Write signal"
+    S_dut[0] = int(dut.state_din[0].value)
+
+    await n_cycles_clock(dut, 1)
+
+    assert int(dut.current_state.value) == int(
+        dut.END_STATE.value
+    ), f"ERROR STATE IN TEST, STATE={int(dut.current_state.value)}"
+
+    for i in range(0, 5):
+        assert (
+            S_dut[i] == expected_State[i]
+        ), f"Error in state {i} expected {hex(expected_State[i])}, calculated={hex(S_dut[i])}"
+
+    for i in range(0, num_blocks):
+        dut._log.info("P_array[{0}] = {1}".format(i, hex(P_array[i])))
+
+    return P_array
+
+
+async def n_cycles_clock(dut, n):
+    for i in range(0, n):
+        await RisingEdge(dut.clk)
+        await FallingEdge(dut.clk)
+
+
+@cocotb.test()
+@cocotb.parametrize(index=range(0, 10))
+async def test(dut, index=0):
+
+    random.seed(index)
+
+    key = random.getrandbits(128)
+    nonce = random.getrandbits(128)
+
+    m = random.randint(1, 3)
+    A_array = [0] * m
+    A_value = 0
+    for i in range(0, m):
+        A_data = random.getrandbits(int(dut.rate.value) * 8)
+        A_array[i] = A_data
+        A_value = A_value + (A_data << (int(dut.rate.value)) * (i * 8))
+
+    n = random.randint(1, 3)
+    C_array = [0] * n
+    C_value = 0
+    for i in range(0, n):
+        C_data = (random.getrandbits(int(dut.rate.value) * 8)) | (
+            (1 << (int(dut.rate.value) * 8) - 1)
+        )
+        C_array[i] = C_data
+        C_value = C_value + (C_data << (int(dut.rate.value)) * (i * 8))
+
+    S_init = [0, 0, 0, 0, 0]
+    S = [0, 0, 0, 0, 0]
+    initial_ascon = ascon_initialize(
+        S_init,
+        int(128),
+        int(dut.rate.value),
+        int(dut.a.value),
+        int(dut.b.value),
+        int(1),
+        key.to_bytes(16, "little"),
+        nonce.to_bytes(16, "little"),
+    )
+
+    dut._log.info(hex(P_value))
+    dut._log.info(n)
+
+    ascon_process_associated_data(
+        S_init,
+        int(dut.b.value),
+        int(dut.rate.value),
+        A_value.to_bytes((m * int(dut.rate.value)), "little"),
+    )
+
+    for k in range(0, 5):
+        S[k] = S_init[k]
+
+    expected_plaintext = ascon_process_ciphertext(
+        S,
+        int(dut.b.value),
+        int(dut.rate.value),
+        C_value.to_bytes((n * int(dut.rate.value)), "little"),
+    )
+
+    setup_dut(dut, S_init)
+    await rst_function_test(dut)
+    await test_ciphertext_data(dut, S, C_array, n)
+
+    dut._log.info(bytes_to_hex(expected_plaintext))
---
+++ ecb_mode/hdl/processing_ciphertext_phase.sv
@@ -0,0 +1,184 @@
+/**
+ * File              : processing_ciphertext_phase.sv
+ * Author            : German C.Quiveu <germancq@dte.us.es>
+ * Date              : 10.09.2026
+ * Last Modified Date: 10.09.2026
+ * Last Modified By  : German C.Quiveu <germancq@dte.us.es>
+ */
+
+module processing_ciphertext_phase #(
+    parameter a = 12,
+    parameter b = 8,
+    parameter rate = 16
+) (
+    input clk,
+    input rst,
+    input [(rate<<3)-1:0] block_i,
+    output [(rate<<3)-1:0] block_o,
+    input start,
+    input feed_block,
+    input last_block,
+    output logic [63:0] state_din[4:0],
+    input [63:0] state_dout[4:0],
+    output logic [0:0] state_w[4:0],
+    output logic [7:0] p_impl_rounds,
+    output logic p_impl_start,
+    output logic p_impl_rst,
+    input p_impl_end,
+    output logic error,
+    output logic end_signal
+);
+
+
+  logic r_block_o_cl;
+  logic r_block_o_w;
+  logic [(rate<<3)-1:0] r_block_o_din;
+  register #(
+      .DATA_WIDTH(rate << 3)
+  ) r_block_o (
+      .clk(clk),
+      .cl(r_block_o_cl),
+      .w(r_block_o_w),
+      .din(r_block_o_din),
+      .dout(block_o)
+  );
+
+  logic [3:0] current_state, next_state, jmp_state;
+  logic r_jmp_state_cl;
+  logic r_jmp_state_w;
+  logic [3:0] r_jmp_state_din;
+
+  register #(
+      .DATA_WIDTH(4)
+  ) r_jmp_state (
+      .clk(clk),
+      .cl(r_jmp_state_cl),
+      .w(r_jmp_state_w),
+      .din(r_jmp_state_din),
+      .dout(jmp_state)
+  );
+
+  localparam IDLE = 0;
+  localparam XOR_DATA = 1;
+  localparam ASCON_PERMUTATION_B_0 = 2;
+  localparam ASCON_PERMUTATION_B_1 = 3;
+  localparam END_FEED = 4;
+  localparam END_FEED_0 = 5;
+  localparam FINAL_STEP_0 = 6;
+  localparam FINAL_STEP_1 = 7;
+  localparam END_STATE = 8;
+  localparam ERROR = 9;
+
+  logic [31:0] j;
+
+  always_comb begin
+    next_state = current_state;
+    for (j = 0; j < 5; j++) begin
+      state_w[j]   = 0;
+      state_din[j] = 0;
+    end
+    p_impl_rounds = b;
+    p_impl_rst = 0;
+    p_impl_start = 0;
+    end_signal = 0;
+    error = 0;
+
+    r_block_o_w = 0;
+    r_block_o_din = 0;
+    r_block_o_cl = 0;
+
+    r_jmp_state_cl = 0;
+    r_jmp_state_w = 0;
+    r_jmp_state_din = current_state;
+    case (current_state)
+      IDLE: begin
+        r_jmp_state_cl = 1;
+        r_block_o_cl   = 1;
+        if (start) begin
+          next_state = XOR_DATA;
+        end
+      end
+      XOR_DATA: begin
+        if (rate == 8) begin
+          state_din[0] = block_i[63:0];
+          r_block_o_din = state_dout[0] ^ block_i[63:0];
+          state_w[0] = 1;
+        end else if (rate == 16) begin
+          state_din[0] = block_i[63:0];
+          state_din[1] = block_i[127:64];
+          r_block_o_din = {(state_dout[1] ^ block_i[127:64]), (state_dout[0] ^ block_i[63:0])};
+          state_w[0] = 1;
+          state_w[1] = 1;
+        end else begin
+          next_state = ERROR;
+        end
+        r_block_o_w = 1;
+        next_state = ASCON_PERMUTATION_B_0;
+        r_jmp_state_w = 1;
+        r_jmp_state_din = END_FEED;
+      end
+      ASCON_PERMUTATION_B_0: begin
+        p_impl_rounds = b;
+        for (j = 0; j < 5; j++) begin
+          state_din[j] = state_dout[j];
+        end
+        p_impl_start = 1;
+        next_state   = ASCON_PERMUTATION_B_1;
+      end
+      ASCON_PERMUTATION_B_1: begin
+        p_impl_rounds = b;
+        for (j = 0; j < 5; j++) begin
+          state_din[j] = state_dout[j];
+        end
+        if (p_impl_end == 1) begin
+          next_state = jmp_state;
+          for (j = 0; j < 5; j++) begin
+            state_w[j] = 1;
+          end
+        end
+      end
+      END_FEED: begin
+        p_impl_rst = 1;
+        next_state = END_FEED_0;
+        if (last_block == 1) begin
+          next_state = FINAL_STEP_0;
+        end
+      end
+      END_FEED_0: begin
+        end_signal = 1;
+        if (feed_block == 1) begin
+          next_state = XOR_DATA;
+        end
+      end
+      FINAL_STEP_0: begin
+
+        state_din[0] = state_dout[0] ^ (1);
+        state_w[0]   = 1;
+
+        // suppose plaintext are multiples of rate
+        // if P_len % rate = 0 
+
+        next_state   = END_STATE;
+
+      end
+      END_STATE: begin
+        end_signal = 1;
+      end
+      ERROR: begin
+        error = 1;
+      end
+
+    endcase
+
+  end
+
+  always_ff @(posedge clk) begin
+    if (rst) begin
+      current_state <= IDLE;
+    end else begin
+      current_state <= next_state;
+    end
+
+  end
+
+endmodule
